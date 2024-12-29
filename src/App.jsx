09664ec643fd4d45cf010f14620ef80df7e60369
bichitrabehera <bichitrabehera.amcec@gmail.com>
2024-12-29 12:03:14 +0530

    new commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <nav>
+        <h3>EvilGPT</h3>
+      </nav>
+      
+      <div className="main-container">
+
+        <div className='text'>
+          How can I serve you my Master ? 😈
+        </div>
+
+        <div className="container">
+          <input type="text" name="" id="" placeholder='Message EvilGPT' />
+          <button type="submit">Send</button>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
